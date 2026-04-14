8d745fb88176b2873aca8ba423d2a165951e0374
geaned <angenispb@mail.ru>
2026-04-14 13:39:05 +0000

    Added 80b3 config

--- examples/run_yagpt_80b3_eagle3_online.sh
+++ examples/run_yagpt_80b3_eagle3_online.sh
@@ -7,8 +7,9 @@ export TORCHINDUCTOR_CACHE_DIR=$ROOT_DIR/cache/compiled_kernels
 # train eagle3 for yagpt
 # Note: --dist-timeout 120 (minutes) prevents NCCL timeout during checkpoint save
 NUM_GPUS=${1:-8}
+TP_SIZE=${2:-2}  # should not be greater than 2 for this arch (due to num_key_value_heads=2)
 MODEL_PATH=/home/geaned/models/hf/yandexgpt-5.1-80b3-202512
-DATASET_PATH=/home/geaned/specforge/train_smol_sample.tsv
+DATASET_PATH=/home/geaned/scripts/train_smol_sample.tsv
 
 torchrun \
     --standalone \
@@ -21,14 +22,14 @@ torchrun \
     --output-dir $ROOT_DIR/yandexgpt-eagle-next \
     --num-epochs 1 \
     --draft-global-batch-size 4 \
-    --draft-micro-batch-size 2 \
+    --draft-micro-batch-size 1 \
     --learning-rate 1e-4 \
     --warmup-ratio 5e-3 \
     --max-length 10240 \
     --chat-template yagpt_custom \
     --cache-dir $ROOT_DIR/cache \
     --embedding-key model.embed_tokens.weight \
-    --tp-size $NUM_GPUS \
+    --tp-size $TP_SIZE \
     --attention-backend flex_attention \
     --ttt-length 3 \
     --report-to tensorboard \
